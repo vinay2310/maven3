90bac859173d69f546991e67a41c61a1b4305b2d
vinay2310 <ping23vinay@gmail.com>
2021-07-30 11:00:07 +0530

    files commited

---
+++ my-app/src/main/java/com/Devops/app/App.java
@@ -0,0 +1,15 @@
+package com.Devops.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name="javatpoint";  
+char ch=name.charAt(10);
+System.out.println(ch);
+    }
+}
